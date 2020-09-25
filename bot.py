b80766edc1a0c9dcffdd880a16da2c1bf9ba9531
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2020-09-25 13:53:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,8 +53,8 @@ mybot = Bot(TOKEN)
 def getimg(case,col):
     global width,height,back
     img = cv2.imread("ChrImages/%s.png"%case)
-    img = np.array(img)
-    img[np.where((img!=[255,255,255]).all(axis=0))] = col
+#     img = np.array(img)
+#     img[np.where((img!=[255,255,255]).all(axis=0))] = col
     cv2.imwrite("ChrImages/chr.png",img)
     cases = Image.open("ChrImages/chr.png")
     back.paste(cases,(width,height))
@@ -124,8 +124,8 @@ def mytexttohand(bot,update):
     def getimg(case,col):
         global width,height,back
         img = cv2.imread("ChrImages/%s.png"%case)
-        img = np.array(img)
-        img[np.where((img!=[255,255,255]).all(axis=0))] = col
+#         img = np.array(img)
+#         img[np.where((img!=[255,255,255]).all(axis=0))] = col
         cv2.imwrite("ChrImages/chr.png",img)
         cases = Image.open("ChrImages/chr.png")
         back.paste(cases,(width,height))
